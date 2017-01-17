63913832d6e667c1a62f8be54ced57b628901c31
josuetorrico <josue.torrico@gmail.com>
2017-01-16 23:44:35 -0500

    Create find-the-point.c
    This code solves the problem titled find the point in hackerrank's practice problems. It utilizes the midpoint formula to obtain the reflaction of a two dimensional point accross another given point. Follow the link below to see the problem.
    Sample input:
    2
    0 0 1 1
    1 1 2 2
    Expected Output:
    2 2
    3 3

---
+++ find-the-point.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    //To solve this problem, I am going to use the midpoint formula to obtain the point r
+    
+    //We need a variable n for the # of points
+    int n;
+    //now some variables for the input
+    int px,py,qx,qy,rx,ry;
+    
+    //let's take the inputs from STDIN
+    scanf("%i",&n);
+    
+    for (int i;i < n;i++){
+        scanf("%i %i %i %i",&px,&py,&qx,&qy);
+        rx = (2 * qx) - px;
+        ry = (2 * qy) - py;
+        printf("%i %i\n",rx,ry);
+    }
+
+    
+    return 0;
+}
